f5d6cbdfc5ac0e42703294ee30e9462afbfda721
anoopano <anoopp998@gmail.com>
2020-11-23 12:33:30 +0530

    Updates

---
+++ precache-manifest.dbc917c2d1d50a80b076adac2113b22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27fd79303c9b41aa21beafd07782384",
+    "url": "/online_shopping/index.html"
+  },
+  {
+    "revision": "09ffa48dcad623d2520f",
+    "url": "/online_shopping/static/css/main.b08f5259.chunk.css"
+  },
+  {
+    "revision": "a8f2cb7a1a3427c0a1b5",
+    "url": "/online_shopping/static/js/2.50ebb46e.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/online_shopping/static/js/2.50ebb46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ffa48dcad623d2520f",
+    "url": "/online_shopping/static/js/main.b6720d2a.chunk.js"
+  },
+  {
+    "revision": "ed190ddd4a669d7ee198",
+    "url": "/online_shopping/static/js/runtime-main.8a71b622.js"
+  }
+]);
